c083360a6951a412fa4d2b76f832ff149781ae85
bassicrob <41550601+bassicrob@users.noreply.github.com>
2025-11-14 00:11:27 -0500

    Update sensor.py
    test for realtime data stream until esp32 crashes

--- components/caliper/sensor.py
+++ components/caliper/sensor.py
@@ -17,10 +17,11 @@ CONFIG_SCHEMA = sensor.sensor_schema(
         accuracy_decimals=2,
     ).extend({
         cv.GenerateID(): cv.declare_id(CaliperSensor),
-    }).extend(cv.polling_component_schema('10s'))
+    }).extend(cv.polling_component_schema('25ms'))
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+
     await sensor.register_sensor(var, config)
